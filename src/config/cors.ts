703ebddc3c4186fc87cd49ad6a055907c2491982
Luis Balcazar <luisbalcares17@gmail.com>
2026-04-11 15:11:57 -0300

    modify cors config

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,12 +5,6 @@ export const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
     const whiteList = [process.env.CLIENT_URL];
 
-    // Permitir requests sin origin (Postman, mismo origen, etc.)
-    if (!origin) {
-      callback(null, true);
-      return;
-    }
-
     if (whiteList.includes(origin)) {
       callback(null, true);
     } else {
